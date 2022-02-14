afd01d70cad93830c9ad4279d37a62205015889c
2021303 <2021303@B211-03>
2022-02-14 10:19:09 +0100

    Add project files.

---
+++ Mtable22/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mtable22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = 0;
+
+            // Ha kapok számot a parancssorból
+            if (args.Length > 0)
+            {
+                // Akkor átalakítom
+                szam = int.Parse(args[0]);
+            }
+            else
+            {
+                Random rnd = new Random();
+                szam = rnd.Next(1, 50);
+            }
+
+            Console.WriteLine(szam * 2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Számkitaláló/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Számkitaláló
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isFelhasznalo = false;
+            Console.WriteLine("Ki jatszik ha jatekos akkor 1");
+            short ki_jatszik = Convert.ToInt16(Console.ReadLine());
+            if(ki_jatszik == 1)
+            {
+                isFelhasznalo = true;
+            }
+
+            if (isFelhasznalo)
+            {
+                Console.WriteLine("adj meg egy szamot fiam");
+                int user_random = Convert.ToInt32(Console.ReadLine());
+                
+                int rand_min = 1;
+                int rand_max = 100;
+                Random random = new Random();
+                
+                int pc_random = 0;
+                while (user_random != pc_random)
+                {
+                    pc_random = random.Next(rand_min, rand_max);
+                    Console.WriteLine("Pc tipp {0}", pc_random);
+                    if (user_random > pc_random)
+                    {
+                        Console.WriteLine("Tul kicsi");
+                        rand_min += 10;
+                        if (rand_min > user_random)
+                        {
+                            rand_min = user_random;
+                        }
+                    }
+                    else if (user_random < pc_random)
+                    {
+                        Console.WriteLine("Tul nagy");
+                        rand_max -= 10;
+                        if (rand_max < user_random)
+                        {
+                            rand_max = user_random;
+                        }
+                    }
+                    else if (user_random == pc_random)
+                    {
+                        Console.WriteLine("Pc nyert");
+                        break;
+                    }
+                }
+             }
+            else
+            {
+                Random random = new Random();
+                int pc_random = random.Next(1, 100);
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Tippeljél");
+                    int tipp = Convert.ToInt32(Console.ReadLine());
+                    if (tipp > pc_random)
+                    {
+                        Console.WriteLine("túl nagy fiam...");
+                    }
+                    else if (tipp == pc_random)
+                    {
+                        Console.WriteLine("szép meló");
+                        Console.WriteLine(pc_random);
+                        break;
+                    }
+                    else if (tipp < pc_random)
+                    {
+                        Console.WriteLine("kevés lesz fiam");
+                    }
+
+                    if(i == 4)
+                    {
+                        Console.WriteLine("Pc szam {0}",pc_random);
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
