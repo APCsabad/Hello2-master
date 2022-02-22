aa339ec8e8c1f3b3eca64f4c4210f6c61af4f06f
Csaba Szemán <2021303@dszcberegszaszi.hu>
2022-02-22 11:40:20 +0100

    vguv

--- bg/Program.cs
+++ bg/Program.cs
@@ -10,6 +10,31 @@ namespace bg
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Adja meg a testtomeget(kg)");
+            double testtomeg = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg a testmagassagot(cm)");
+            double testmagassag = int.Parse(Console.ReadLine());
+            double bmi = 0;
+            bmi = testtomeg / (testmagassag * testmagassag);
+            Console.WriteLine(bmi);
+
+            if (bmi <= 18.5)
+            {
+                Console.WriteLine("sovány");
+            }
+            else if (bmi >= 18.6 && bmi <=25)
+            {
+                Console.WriteLine("normál");
+            }
+            else if (bmi >= 26 && bmi <= 30)
+            {
+                Console.WriteLine("kövér");
+            }
+            else if (bmi > 30)
+            {
+                Console.WriteLine("dagadt");
+            }
+
         }
     }
 }
---
+++ pont tavolsag/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pont_tavolsag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adja meg az x1 pontot: )");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adja meg az x2 pontot: )");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adja meg az y1 pontot: )");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adja meg az y2 pontot: )");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            double tav = 0;
+            tav = Math.Sqrt((1x - 2x, 2) + (1y - 2y, 2));
+
+
+        }
+    }
+}
