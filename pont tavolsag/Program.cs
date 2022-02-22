d26bca26d6eccc0a6a90468b6a8f098d8692b063
Csaba Szemán <2021303@dszcberegszaszi.hu>
2022-02-22 12:30:04 +0100

    modositasok modositasa

--- pont tavolsag/Program.cs
+++ pont tavolsag/Program.cs
@@ -10,18 +10,19 @@ namespace pont_tavolsag
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Adja meg az x1 pontot: )");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Adja meg az x2 pontot: )");
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Adja meg az y1 pontot: )");
-            int y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Adja meg az y2 pontot: )");
-            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adja meg az x1 pontot: ");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg az x2 pontot: ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg az y1 pontot: ");
+            double y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg az y2 pontot: ");
+            double y2 = double.Parse(Console.ReadLine());
             double tav = 0;
-            tav = Math.Sqrt((1x - 2x, 2) + (1y - 2y, 2));
+            tav = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
 
-
+            Console.WriteLine("Két pont távolsága:{0}", tav);
+            Console.ReadKey();
         }
     }
 }
