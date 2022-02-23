87e3255e5c27670b76d7e9f98ca3da7c8148a2d2
Csaba Szemán <2021303@dszcberegszaszi.hu>
2022-02-23 11:40:22 +0100

    sd

--- Dinnye/Program.cs
+++ Dinnye/Program.cs
@@ -10,7 +10,13 @@ namespace Dinnye
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Adja meg a dinnye átmérőjét!(cm)");
+            double csomag = 0;
             double testtomeg = int.Parse(Console.ReadLine());
+            csomag = Math.PI *testtomeg*2 + 60;
+            Console.WriteLine("Önnek {0}cm szalagra van szüksége.",csomag);
+            Console.ReadKey();
+
         }
     }
 }
