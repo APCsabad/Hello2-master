86af15d99b2c3cfde8b5f2229df5329f0cde4cb8
APCsabad <90307225+APCsabad@users.noreply.github.com>
2022-02-23 10:53:16 +0100

    Add files via upload

---
+++ konyv/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konyv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vege = "0";
+            int konyvo = 0;
+            while (vege == "0")
+            {
+                Console.WriteLine("Adj meg a könyv címét!");
+                string konyvc = Console.ReadLine();
+                if (konyvc == "") break;
+                Console.WriteLine("Add meg a könyv oldalszámait!");
+                if(konyvc != "")  konyvo = Convert.ToInt32(Console.ReadLine());
+                if (konyvo > 150)
+                {
+                    Console.WriteLine("A(z) {0} hosszú terjedelmű könyv.", konyvc);
+                }
+                else
+                {
+                    Console.WriteLine("A(z) {0} rövid terjedelmű könyv.", konyvc);
+                }
+                if (konyvc =="")
+                {
+                    vege = "";
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ szambekeres max/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szambekeres_max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("írj be számokat, ha 0-t írsz akkor megáll a program és kiírja a legnagyobbat közülük.");
+            int max = 0;
+            int valtozo = -1;
+            do
+            {
+                valtozo=Convert.ToInt32(Console.ReadLine());
+                if (max < valtozo) max = valtozo;
+                
+            } while (valtozo != 0);
+            Console.WriteLine("A legnagyobb szám: {0}", max);
+            Console.ReadKey();
+        }
+
+        }
+    }
+
