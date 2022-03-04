13c17ac64963a4821d76de807083b2633061e167
Csaba Szemán <2021303@dszcberegszaszi.hu>
2022-03-04 11:36:10 +0100

    weq

--- körclass/Program.cs
+++ körclass/Program.cs
@@ -6,31 +6,51 @@ using System.Threading.Tasks;
 
 namespace körclass
 {
-    class kör
-    {
+    class Kör
+    {   //henger osztály be kell kérni a magasságát, metódust írni a térfogatra az alapterületre
         // Osztályváltozók (properties) körs sugár  beolvasás majd kör terület kerület
-        private double körsugar;
-
+        protected double körsugar;
+        public Kör() { } 
 
       
-        public void setSugar() { körsugar = Convert.ToDouble(Console.ReadLine()); }
-        public double getSugar() { return körsugar; }
-        public double getTerület() { return  Math.PI * (körsugar * körsugar); }
+        public void setSugar() { this.körsugar = Convert.ToDouble(Console.ReadLine()); }
+        public double getSugar() { return this.körsugar; } 
+        public double getTerület() { return  Math.PI * (this.körsugar * this.körsugar); }
        
-        public double getKerület() { return 2*Math.PI * körsugar; }
+        public double getKerület() { return 2*Math.PI * this.körsugar; }
       
+    }
+    class Henger : Kör {
+        
+        private double magassag;
+        public Henger() : base() { }
+        public void Magasság() 
+        { 
+            this.magassag = Convert.ToDouble(Console.ReadLine());
+        }
+        public double getMagasság() { return this.magassag; }
+
+        public double getTérfogat() { return Math.PI * (this.körsugar * this.körsugar) * this.magassag; }
+        public double getalapterület() { return 2 * Math.PI * this.körsugar * this.magassag; }
+
+
     }
 
     class Program
         {
             static void Main(string[] args)
         {
-            kör kör1 = new kör();
-            Console.WriteLine("Adja meg a kör sugarát");
-            kör1.setSugar();
-            Console.WriteLine("A kör sugara: {0}", kör1.getSugar());
-            Console.WriteLine("A kör kerülete: {0}", kör1.getKerület());
-            Console.WriteLine("A kör területe: {0}", kör1.getTerület());
+            Kör kör1 = new Kör();
+            Henger henger1 = new Henger();
+            Console.WriteLine("Adja meg a kör sugarát!");
+            henger1.setSugar();
+            Console.WriteLine("Adja meg a henger magasságát!");
+            henger1.Magasság();
+            Console.WriteLine("A kör sugara: {0}", henger1.getSugar());
+            Console.WriteLine("A kör kerülete: {0}", henger1.getTerület());
+            Console.WriteLine("A henger magassága: {0}", henger1.getMagasság());
+            Console.WriteLine("A henger térfogata: {0}", henger1.getTérfogat());
+            Console.WriteLine("A henger alapterülete: {0}", henger1.getalapterület());
             Console.ReadKey();
         }
         }
