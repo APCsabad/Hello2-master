dcc4ad14aea47b265226b14b459f5a79dab01639
Csaba Szemán <2021303@dszcberegszaszi.hu>
2022-03-18 10:21:47 +0100

    8cx

--- Programozási tételek/Program.cs
+++ Programozási tételek/Program.cs
@@ -10,22 +10,11 @@ namespace Programozási_tételek
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Adja meg hány elem szerepeljen? A megadható érték nem lehet nagyobb, mint 200! Ha ennél nagyobbat ad meg, akkor a program meghatároz véletlen szerűen egy értéket 100 és 200 között.  ");
-            int a = Convert.ToInt32(Console.ReadLine());
-            Random random = new Random();
-            int negatívak = 0;
-            if (a > 200) {
-                a = random.Next(100, 200);
-            }
-            int[] tomb = new int[a];
-            for (int i = 0; i < a; i++)
-            {
-                tomb[i] = random.Next(-100, 100);
+            Tetelek tetel = new Tetelek();
 
-                if (tomb[i] < 0) {
-                    negatívak++;
-                }
-            }
+            Console.WriteLine("A tömb elemeinek összege: {0} ", tetel.osszeg());
+            Console.WriteLine("A tömbben {0} negatív szám van.", tetel.negativ());
+            Console.WriteLine(tetel.osszehasonlitas());
             Console.ReadKey();
 
         }
---
+++ Programozási tételek/Tetelek.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programozási_tételek
+{
+    class Tetelek
+    {
+        private int[] tomb;
+        public Tetelek() {
+            Console.WriteLine("Adja meg hány elem szerepeljen? A megadható érték nem lehet nagyobb, mint 200! Ha ennél nagyobbat ad meg, akkor a program meghatároz véletlen szerűen egy értéket 100 és 200 között.  ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+
+            if (a > 200)
+            {
+                a = random.Next(100, 200);
+            }
+
+            tomb = new int[a];
+
+            for (int i = 0; i < a; i++)
+            {
+                tomb[i] = random.Next(-100, 100);              
+            }
+            
+        }
+
+        public int osszeg()
+        {
+            int tombossz = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tombossz = tombossz + tomb[i];
+            }
+
+            return tombossz;
+        }
+        public int negativ()
+        {
+            int negativak = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+
+                if (tomb[i] < 0)
+                {
+                    negativak++;
+                }
+            }
+
+            return negativak;
+        }
+        public bool osszehasonlitas()
+        {
+            Console.WriteLine("Adjon meg egy számot és a program megnézi hogy szerepel-e a tömbben.");
+            int b = Convert.ToInt32(Console.ReadLine());
+            bool van = false;
+            
+            for (int i = 0; i < tomb.Length; i++)
+            {
+
+                if (tomb[i] == b)
+                {
+                    van = true;
+                    break;
+                }
+            }
+
+            return van;
+        }
+
+
+    }
+}
