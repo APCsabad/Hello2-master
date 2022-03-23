311f4962b1a65bafc89c1357e6558cd77153dc2c
Csaba Szemán <2021303@dszcberegszaszi.hu>
2022-03-23 10:17:55 +0100

    ldagdagbnadjgbndaignbadgndahiadghdoadsngadgiadhioadnoadoashgiabgoushgouadngosadojhnodanhipadjhndanhadnohadhodaanhadhnpj

--- Programozási tételek2/Class1.cs
+++ Programozási tételek2/Class1.cs
@@ -9,10 +9,11 @@ namespace Programozási_tételek
     class Tetelek
     {
         private int[] tomb;
+        private int a;
         public Tetelek()
         {
             Console.WriteLine("Adja meg hány elem szerepeljen? A megadható érték nem lehet nagyobb, mint 200! Ha ennél nagyobbat ad meg, akkor a program meghatároz véletlen szerűen egy értéket 100 és 200 között.  ");
-            int a = Convert.ToInt32(Console.ReadLine());
+            a = Convert.ToInt32(Console.ReadLine());
             Random random = new Random();
 
             if (a > 200)
@@ -52,13 +53,14 @@ namespace Programozási_tételek
                 return index;
             }
         }
-        public int negativ()
+        public void kivalogatas()
         {
             Console.WriteLine("Adjon meg egy számot.");
             int b = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Adjon meg egy relációs jelet.");
             string a = Console.ReadLine();
-            tomb2 = new int[b];
+
+
             if (a == "<")
             {
                 for (int i = 0; i < tomb.Length; i++)
@@ -66,23 +68,75 @@ namespace Programozási_tételek
 
                     if (b < tomb[i])
                     {
-                       tomb2 index = tombi
-                            index++
-
-                        tomb[b] = tomb[i]; b++; 
-
+                        Console.WriteLine(tomb[i]);
                     }
                 }
             }
+            if (a == ">")
+            {
+                for (int i = 0; i < tomb.Length; i++)
+                {
 
+                    if (b > tomb[i])
+                    {
+                        Console.WriteLine(tomb[i]);
+                    }
+                }
+            }
+            if (a == "=")
+            {
+                for (int i = 0; i < tomb.Length; i++)
+                {
 
-
-            return ;
+                    if (b == tomb[i])
+                    {
+                        Console.WriteLine(tomb[i]);
+                    }
+                }
+            }
         }
 
-        public int osszehasonlitas()
+        public void szetvalogatas()
         {
-            Console.WriteLine("Adjon meg egy számot és a program megnézi hogy hanyadik helyen szerepel.");
+            Console.WriteLine("Adjon meg egy számot.");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adjon meg egy relációs jelet.");
+            string jel = Console.ReadLine();
+            int[] tomb1 = new int[a]; //nagyobb elemek
+            int[] tomb2 = new int[a]; //kisebb elemek
+            int tomb1_i = 0, tomb2_i = 0;
+
+
+            if (jel == "<")
+            {
+                for (int i = 0; i < tomb.Length; i++)
+                {
+                    if (b < tomb[i])
+                    {
+                        tomb1[tomb1_i++] = tomb[i];
+                    }
+                    else
+                    {
+                        tomb2[tomb2_i++] = tomb[i];
+                    }
+                }
+            }
+            Console.WriteLine("A megadott relációs jel és szám alapján ezek a számok nagyobbak mint a szám:");
+
+            for (int i = 0; i < tomb1.Length; i++)
+            {
+                Console.WriteLine(tomb1[i]);
+            }
+           
+            Console.WriteLine("A megadott relációs jel és szám alapján ezek a számok kisebbek mint a szám:");
+            for (int i = 0; i < tomb2.Length; i++)
+            {
+                Console.WriteLine(tomb2[i]);
+            }
+        }
+        public void osszehasonlitas()
+        {
+            Console.WriteLine("Adjon meg egy számot és a program megnézi, hogy benne van e a tömbbe és hogy hanyadik helyen szerepel.");
             int b = Convert.ToInt32(Console.ReadLine());
             bool van = false;
             int index = 0;
@@ -94,12 +148,13 @@ namespace Programozási_tételek
                 {
                     index = i;
                     van = true;
+                    Console.WriteLine("A szám szerepel a tömbben és a {0}. helyen szerepel.", index);
                     break;
                 }
             }
 
             
-            return index;
+            
         }
 
 
--- Programozási tételek2/Program.cs
+++ Programozási tételek2/Program.cs
@@ -13,9 +13,11 @@ namespace Programozási_tételek
             Tetelek tetel = new Tetelek();
 
             Console.WriteLine(tetel.kivalasztas());
-            Console.WriteLine("A tömbben {0} negatív szám van.", tetel.negativ());
-            Console.WriteLine(tetel.osszehasonlitas());
+            //tetel.kivalogatas(); 
+            tetel.szetvalogatas();
+            //tetel.osszehasonlitas();
             Console.ReadKey();
+            
 
         }
 
