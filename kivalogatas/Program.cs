74a1fb50486ce86362cf8902cb39f03eef9270c7
Csaba Szemán <2021303@dszcberegszaszi.hu>
2022-03-28 11:21:38 +0200

    adf

---
+++ kivalogatas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kivalogatas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            bool szam = true;
+            Console.WriteLine("Adj meg egy számot!");
+            while (szam)
+            {
+                try
+                {
+                    a = Convert.ToInt32(Console.ReadLine());
+                    szam = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Adj meg egy számot!");
+                    szam = true;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
