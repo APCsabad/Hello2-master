ad3437056552c40f39a6a83f3ad61599689f2d06
Csaba Szemán <2021303@dszcberegszaszi.hu>
2022-03-31 09:58:25 +0200

    asdwa

---
+++ Binaris/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binaris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
