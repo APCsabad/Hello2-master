aea348f89c040530d8fb1bf2c94ca379e941dbf8
Csaba Szemán <2021303@dszcberegszaszi.hu>
2022-04-05 12:18:23 +0200

    dsa

---
+++ Lista/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int 
+            Random random = new Random();
+            a = random.Next(10, 100);
+        }
+    }
+}
