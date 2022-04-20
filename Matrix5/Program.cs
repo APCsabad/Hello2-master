5aeb9d09ed7a32fa563b0e3fabe0afa4c6568307
Csaba Szemán <2021303@dszcberegszaszi.hu>
2022-04-20 13:22:24 +0200

    osdjghuoebngebgjngdbnghaglndojghbaegdandkbgaogbldjbgadgnadignmadjkbngisgnjadngljagnkdjgékasngjkbdgankgnsdjgbasdgndljgnjadgnjkadngjdgfnaklgnjlasdgnasdklnmvldngfjasjdnsajgnjfsngjfbjdlbvhksdbgdbnkxcbnjsdbnébndjlbnbnyxjlbvylbmnkyébnjydnblydbhkybvydjlnbéykdnbjlybvgylb   pidgdnbgdsbngonbb

--- Matrix5/Program.cs
+++ Matrix5/Program.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MatrixBill4
+namespace Matrix5
 {
     class Program
     {
-        public static void kiir(int[,] tömb ) {
+        public static void kiir(int[,] tömb)
+        {
             for (int i = 0; i < tömb.GetLength(0); i++)
             {
                 for (int j = 0; j < tömb.GetLength(1); j++)
@@ -26,21 +27,21 @@ namespace MatrixBill4
             int sor = 0;
             int oszlop = 0;
             string folytatas = "igen";
-            
-                Console.WriteLine("Adja meg hány soros legyen a mátrixa");
+
+            Console.WriteLine("Adja meg hány soros legyen a mátrixa");
             sor = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Adja meg hány oszlopos legyen a mátrixa");
             oszlop = Convert.ToInt32(Console.ReadLine());
-             
+
             int[,] mátrix = new int[sor, oszlop];
             while (folytatas == "igen")
             {
                 Console.WriteLine("Írja be vesszővel elválasztva, hogy a maga által megadott sor, oszlop, milyen értéket adjon. Formátum: sorszám,oszlopszám,érték");
-            string bekeres = Console.ReadLine();
-            string[] beker = bekeres.Split(',');
-            sor = Convert.ToInt32(beker[0]);
-            oszlop = Convert.ToInt32(beker[1]);
-            int ertek = Convert.ToInt32(Math.Round(Convert.ToDouble(beker[2])));
+                string bekeres = Console.ReadLine();
+                string[] beker = bekeres.Split(',');
+                sor = Convert.ToInt32(beker[0]);
+                oszlop = Convert.ToInt32(beker[1]);
+                int ertek = Convert.ToInt32(Math.Round(Convert.ToDouble(beker[2])));
 
 
 
@@ -58,9 +59,21 @@ namespace MatrixBill4
                 folytatas = Console.ReadLine();
 
             }
-                
+            for (int i = 0; i < mátrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < mátrix.GetLength(1); j++)
+                {
+                    if (mátrix[i, j] == 0)
+                    {
+                        mátrix[i, j] = -1;
+                    }
 
-            
+
+                }
+                
+            }
+
+            kiir(mátrix);
             Console.ReadKey();
         }
     }
--- Matrix6/Program.cs
+++ Matrix6/Program.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MatrixBill4
+namespace Matrix6
 {
     class Program
     {
-        public static void kiir(int[,] tömb ) {
+        public static void kiir(int[,] tömb)
+        {
             for (int i = 0; i < tömb.GetLength(0); i++)
             {
                 for (int j = 0; j < tömb.GetLength(1); j++)
@@ -26,21 +27,22 @@ namespace MatrixBill4
             int sor = 0;
             int oszlop = 0;
             string folytatas = "igen";
-            
-                Console.WriteLine("Adja meg hány soros legyen a mátrixa");
+            int sum = 0;
+
+            Console.WriteLine("Adja meg hány soros legyen a mátrixa");
             sor = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Adja meg hány oszlopos legyen a mátrixa");
             oszlop = Convert.ToInt32(Console.ReadLine());
-             
+
             int[,] mátrix = new int[sor, oszlop];
             while (folytatas == "igen")
             {
                 Console.WriteLine("Írja be vesszővel elválasztva, hogy a maga által megadott sor, oszlop, milyen értéket adjon. Formátum: sorszám,oszlopszám,érték");
-            string bekeres = Console.ReadLine();
-            string[] beker = bekeres.Split(',');
-            sor = Convert.ToInt32(beker[0]);
-            oszlop = Convert.ToInt32(beker[1]);
-            int ertek = Convert.ToInt32(Math.Round(Convert.ToDouble(beker[2])));
+                string bekeres = Console.ReadLine();
+                string[] beker = bekeres.Split(',');
+                sor = Convert.ToInt32(beker[0]);
+                oszlop = Convert.ToInt32(beker[1]);
+                int ertek = Convert.ToInt32(Math.Round(Convert.ToDouble(beker[2])));
 
 
 
@@ -58,9 +60,23 @@ namespace MatrixBill4
                 folytatas = Console.ReadLine();
 
             }
-                
+            for (int i = 0; i < mátrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < mátrix.GetLength(1); j++)
+                {
+                    if (mátrix[i, j] == 0)
+                    {
+                        mátrix[i, j] = -1;
+                        sum++;
+                    }
 
-            
+
+                }
+
+            }
+
+            kiir(mátrix);
+            Console.WriteLine("Ennyi hely maradt üresen:{0}",sum);
             Console.ReadKey();
         }
     }
