4ca7baa2fa544766604f6bdf07c6a76f656265a1
Csaba Szemán <2021303@dszcberegszaszi.hu>
2022-04-20 11:39:59 +0200

    ÁNYÁDATMÁR

--- MatrixBill4/Program.cs
+++ MatrixBill4/Program.cs
@@ -21,8 +21,36 @@ namespace MatrixBill4
             int[,] mátrix = new int[sor, oszlop];
             Console.WriteLine("Írja be vesszővel elválasztva, hogy a maga által megadott sor, oszlop, milyen értéket adjon. Formátum: sorszám,oszlopszám,érték");
             string bekeres = Console.ReadLine();
+            string[] beker = bekeres.Split(',');
+            sor = Convert.ToInt32(beker[0]);
+            oszlop = Convert.ToInt32(beker[1]);
+            int ertek = Convert.ToInt32(beker[2]);
+            string a = " ";
 
+            while (a == "esc")
+            {
 
+            
+            if (mátrix[sor, oszlop] == 0)
+            {
+                mátrix[sor, oszlop] = ertek;
+            }
+            else {
+                Console.WriteLine("Ez a hely már foglalt.");
+
+            }
+            for (int i = 0; i < mátrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < mátrix.GetLength(1); j++)
+                {
+                    Console.Write(mátrix[i, j]+ " ");
+
+                }
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
         }
     }
 }
