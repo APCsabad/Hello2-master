62bda58bc646f84185d08a0e69a04b36ad618b22
Csaba Szemán <2021303@dszcberegszaszi.hu>
2022-05-27 10:33:28 +0200

    sdgaegeagh

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -10,43 +10,9 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-            // Változók
-            int szam1;
-            int szam2;
-            char muvjel;
-            float eredm = 0;
-            string uzen = "A művelet eredménye: ";
-
-            // Beolvasások
-            szam1 = int.Parse(Console.ReadLine());
-            szam2 = int.Parse(Console.ReadLine());
-            muvjel = Convert.ToChar(Console.Read());
-
-            switch (muvjel)
-            {
-                case '+':
-                    eredm = szam1 + szam2;
-                    break;
-
-                case '-':
-                    eredm = szam1 + szam2;
-                    break;
-
-                case '*':
-                    eredm = szam1 + szam2;
-                    break;
-
-                case '/':
-                    eredm = szam1 + szam2;
-                    break;
-
-                default:
-                    uzen = "Hibás műveleti jel";
-                    break;
-            }
-
-            Console.WriteLine(uzen + eredm);
-            Console.ReadKey();
+            Kalkulatorr cal = new Kalkulatorr();
+            cal.beker();
+            cal.kiir();
         }
     }
 }
