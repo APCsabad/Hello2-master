7c33da8530ddcb58eea82a39dfbda6ce791bf49d
Csaba Szemán <2021303@dszcberegszaszi.hu>
2022-05-31 08:55:50 +0200

    kegzvizgbvzuricrftyswayí123456789

---
+++ minmax/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMax {
+    public class Program {
+        public static void Main(string[] args) {
+
+            Adat adat = new Adat();
+
+            Console.WriteLine("Min: " + adat.min());
+            Console.WriteLine("Max: " + adat.max());
+
+            Console.ReadKey();
+        }
+    }
+}
