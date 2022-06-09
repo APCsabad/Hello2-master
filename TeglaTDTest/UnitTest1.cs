f7a83dcafe4f394fb28e6cf7f82cd46992bbfecc
Csaba Szemán <2021303@dszcberegszaszi.hu>
2022-06-09 13:06:22 +0200

    kodnfuhdbgi9

---
+++ TeglaTDTest/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TéglaTD;
+
+namespace TeglaTDTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestFelszin()
+        {
+            Tegla tegla = new Tegla(1, 2, 3);
+            int vart = 9;
+            int kapott = tegla.getFelszin();
+            Assert.AreNotEqual(vart, kapott);
+        }
+
+        [TestMethod]
+        public void felszinTestRossz()
+        {
+            Tegla tegla = new Tegla(1, 2, 3);
+            int vart = 6;
+            int kapott = tegla.getFelszin();
+            Assert.AreNotEqual(vart, kapott);
+
+        }
+        [TestMethod]
+        public void tesztterfogat()
+        {
+            Tegla tegla = new Tegla(2, 3, 4);
+            int vart = 24;
+            int kapott = tegla.getTerfogat();
+            Assert.AreEqual(vart, kapott);
+
+        }
+        public void tesztterfogatrossz()
+        {
+            Tegla tegla = new Tegla(1, 2, 3);
+            int vart = 17;
+            int kapott = tegla.getTerfogat();
+            Assert.AreNotEqual(vart, kapott);
+
+        }
+    }
+}
---
+++ TéglaTD/Tegla.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TéglaTD
+{
+    public class Tegla
+    {
+        private int szel, hossz, magas;
+        public Tegla(int szel,int hossz, int magas) {
+            this.szel = szel;
+            this.hossz = hossz;
+            this.magas = magas;
+        }
+        public int getFelszin() {
+
+            return 2 * (szel * magas + szel * hossz + magas * szel); }
+
+        public int getTerfogat() {
+
+            return magas * szel * hossz;
+        }
+    
+    
+    }
+
+    }
+
